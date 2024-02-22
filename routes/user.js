75d9ee02e6e164ca99562e3596aa0ef0bcbeeb0d
Matheus Julidori <matheus_julidori@yahoo.com.br>
2024-02-22 20:40:54 -0300

    User Routes

--- routes/user.js
+++ routes/user.js
@@ -1,30 +1,19 @@
 import { Router } from "express";
+import { getAllUsers,createUser,getUser,deleteUser,updateUser,loginUser } from "../controllers/user.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.send("Return all users");
-} );
+router.get("/", getAllUsers);
 
-router.get("/:id", (req, res) => {
-    res.send(`Return user with id ${req.params.id}`);
-})
+router.get("/:id", getUser)
 
-router.post("/", (req, res) => {
-    res.send("Create a new user");
-})
+router.post("/", createUser)
 
-router.put("/:id", (req, res) => {
-    res.send(`Update user with id ${req.params.id}`);
-})
+router.put("/:id", updateUser)
 
-router.delete("/:id", (req, res) => {
-    res.send(`Delete user with id ${req.params.id}`);
-})
+router.delete("/:id",deleteUser)
 
-router.post("/login", (req, res) => {
-    res.send("Login user");
-})
+router.post("/login", loginUser)
 
 
 export default router;
